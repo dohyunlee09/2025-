d57ed5ee978eb077aef76485b7f26eb26b11005f
dohyunlee09 <dohyunlee0916@gmail.com>
2025-08-09 11:47:20 +0900

    사옹자 입력 및 출력 실습

--- qt.py
+++ qt.py
@@ -1,2 +1,10 @@
 print(1)
 print(2)
+
+#사용자에게 입력받은 값을 바로 정수로 바꾸어 user_input 변수에 대입
+user_input = int(input("숫자를 입력하고 엔터를 누르세요:)"))
+print(int(user_input+7))
+
+#사용자에게 입력받은 값을 user_input 변수에 대입해서 연산할때 정수형으로 바꿔서 연산
+user_input = input("숫자를 입력하고 엔터를 누르세요:")
+print(int(user_input)+8)
