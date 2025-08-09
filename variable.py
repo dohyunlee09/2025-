e4c2d4546bb335134dbfb8277e61a90d32321ce3
dohyunlee09 <dohyunlee0916@gmail.com>
2025-08-09 11:12:38 +0900

    기본 데이터 형태 학습

---
+++ variable.py
@@ -0,0 +1,17 @@
+x =3
+y =x+3
+
+print (y+2)
+
+x=[1,2,3,4,5] #list
+print(x)
+y=3
+print(x[0])
+print(x[y])
+
+x = { 'a':1, 'b':2} #key;value 탐색같은 느낌
+print(x)
+print(x['a'])
+
+x ={ 'a':[1,2,3] 'b':2}
+print(x)
