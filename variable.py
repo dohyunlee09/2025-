37321f90443b9e91e0e5a296264dd44161349293
dohyunlee09 <dohyunlee0916@gmail.com>
2025-08-30 10:50:35 +0900

    list 가공하는 방법

--- variable.py
+++ variable.py
@@ -9,11 +9,37 @@ y=3
 print(x[0])
 print(x[y])
 
+x = x*2
+print("x* 2 :", x)
+
+x=[1,2,3,4,5]
+#slicing
+print(x[2:5])
+print("append========")
+x.append(7)
+print(x)
+print("insert========")
+x.insert(1,9)
+print(x)
+print("remove========")
+x.remove(1) #인덱스가 아닌 함수를 지우는 함수
+print(x)
+print("9->6=======")
+x[0] = 6
+print(x)
+
+#참조할 수 없는 인덱스 위치를 참조했을때
+
+print("배열의 길이=======")
+print(len(x))
+
+print("배열의 마지막 인덱스=====")
+print(len(x)-1)
 x = { 'a':1, 'b':2} #key;value 탐색같은 느낌
 print(x)
 print(x['a'])
 
-x ={ 'a':[1,2,3] 'b':2}
+x ={ 'a':[1,2,3],'b':2}
 print(x)
 
 example = {
