cb96f98597683dfcf780a7bd9a1e3069dd1a6811
dohyunlee09 <dohyunlee0916@gmail.com>
2025-08-30 10:07:31 +0900

    논리연산자 및 연산자 복합 사용

--- operator.py
+++ operator.py
@@ -51,3 +51,22 @@ print("array_x + array_y :" , array_z)
 array_z = array_x * array_y[0] #해당 인덱스에 있는 요소가 정수형일때만 가능
 print("array_x * array_y[0] :" , array_z)
 
+report_card = {
+    "국어":3 ,
+    "수학":2 ,
+    "영어":6 ,
+    "물리":1 ,
+    "화학":2 ,
+    "생명과학" 1,
+}
+
+can_apply = report_card["국어"] < 3 and report_card["수학"] < 3 and report_card["영어"] < 3
+print("지원가능여부 :", can_apply)
+
+#3합 6
+total_score = report_card["국어"] +report_card["수학"]+report_card["영어"] <= 6
+print("3합 6 :", total_score )
+
+#과학 영재반
+can_apply_class_for_the_gifted =report_card["물리"] ==1 or report_card["화학"] == 1 or report_card["수학"]
+print("영재반 지원가능여부 :" , can_apply_class_for_the_gifted)
