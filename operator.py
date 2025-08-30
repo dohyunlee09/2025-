7dff190b5a60c63630fba426e9613e7bc8dfed9d
dohyunlee09 <dohyunlee0916@gmail.com>
2025-08-30 09:37:00 +0900

    대입연산자,사칙연산

---
+++ operator.py
@@ -0,0 +1,29 @@
+x = 10
+y = 15
+
+z= x == y
+print("x == y : ", z)
+z = x != y  #!는 같지않다
+print("x != y : ", z)
+z= x > y
+print("x > y :" ,z)
+z= x >= y
+print("x >= y :" ,z)
+z= x < y
+print("x < y :" ,z)
+z= x <= y
+print("x <= y :" ,z)
+
+#사칙연산
+z = x +y
+print("x + y :" ,z)
+z = x -y
+print("x - y :" ,z)
+z = x * y # 곱셈
+print("x * y :" ,z)
+z = x  /y #나눗셈
+print("x / y :" ,z)
+z = x // y  #나머지
+print("x // y :" ,z)
+z = x ** y #제곱
+print("x ** y :" ,z)
