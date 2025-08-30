cfcef9877b376a0efd3bca9f9a653959362f3c59
dohyunlee09 <dohyunlee0916@gmail.com>
2025-08-30 09:46:37 +0900

    문자 형태의 연산

--- operator.py
+++ operator.py
@@ -1,6 +1,6 @@
 x = 10
 y = 15
-
+#대입연산자
 z= x == y
 print("x == y : ", z)
 z = x != y  #!는 같지않다
@@ -27,3 +27,16 @@ z = x // y  #나머지
 print("x // y :" ,z)
 z = x ** y #제곱
 print("x ** y :" ,z)
+
+str_x = "hello"
+str_y = ("python")
+
+str_z =str_x + str_y
+print("str_x + str_y:" , str_z)
+
+# unsupported operand type(s) for -: 'str' and 'str'
+
+str_z = str_x * 2
+print(str_x * 2:" ,str_z)
+
+
