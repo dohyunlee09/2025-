ea9e3ff87623d0783e0db0f72ad6410da805ecf0
dohyunlee09 <dohyunlee0916@gmail.com>
2025-11-01 11:18:20 +0900

    조건문

---
+++ 2 control-statement.py
@@ -0,0 +1,18 @@
+score = int(input("점수를 입력하세요."))
+
+if score >100 or score <0 :
+    print("정상적인 점수 범위가 아닙니다.")
+else :
+if score > 60:
+    print("합격입니다.")
+elif score >=60:
+    print("재시험 응시가 필요합니다.")
+else:
+    print("불합격입니다.")
+
+if score <= 100:
+    if score >= 0:
+        print("진짜 합격입니다.")
+#13번 줄부터 15번은 그저 범위안에 드는 숫자에대한 정보를 더 주지 범위를 벗어나는 숫자에게는 할당되는 것이 없다
+while True:
+    user_input = input("답을 입력하세요."0)
