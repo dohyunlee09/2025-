e574771bf53c185d2dafb00301606ae42f1068cd
dohyunlee09 <dohyunlee0916@gmail.com>
2025-11-22 11:31:24 +0900

    문제 제출

--- 2 control-statement.py
+++ 2 control-statement.py
@@ -62,30 +62,69 @@ def print_arr_type():
 #arr타입 함수호출로 주석 대체
 
 #수학
-test = [{ 'answer' : [1 , 3, 2 ,4, 5 , 3, 1, 2, 3,4]},
-{ 'answer' : [1 , 3, 2 ,4, 5 , 3, 1, 2, 3,4]},
-{ 'answer' : [1 , 3, 2 ,4, 5 , 3, 1, 2, 3,4]},
-{ 'answer' : [1 , 3, 2 ,4, 5 , 3, 1, 2, 3,4]},
-{ 'answer' : [1 , 3, 2 ,4, 5 , 3, 1, 2, 3,4]},
-{ 'answer' : [1 , 3, 2 ,4, 5 , 3, 1, 2, 3,4]},
-{ 'answer' : [1 , 3, 2 ,4, 5 , 3, 1, 2, 3,4]}]
+test = [
+    {
+        'name': 'aaa',
+        'number': 10101,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    },
+    {
+        'name': 'bbb',
+        'number': 10102,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    },
+    {
+        'name': 'ccc',
+        'number': 10103,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    },  {
+        'name': 'ddd',
+        'number': 10104,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    },
+    {
+        'name': 'eee',
+        'number': 10105,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    }
+]
 
 
 
-a =         [1 , 3, 2 ,4, 5 , 3, 1, 2, 3,4]
-correct_answer = [1 , 5, 2 ,4, 5 , 3, 1, 2, 3,4]
-def print_fortype():
+staudent_answer =         [1 , 3, 2 ,4, 5 , 3, 1, 2, 3,4]
+correct_answer = correct_answer = {
+    'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+}
+def print_fortype(student_answer,correct_answer):
     first_score = 0
-    for (student,correct) in zip(a,correct_answer):
+    for (student,correct) in zip(student_answer,correct_answer):
         print(student , '/' , correct)
         if student == correct:
             first_score = first_score+10 #여기에 있어요
 
-    print(first_score
-                  )
+    print(first_score)
+    return first_score
 def print_fortype2():
     first_score = 100
-    for (student,correct) in zip(a,correct_answer):
+    for (student,correct) in zip(staudent_answer,correct_answer):
         print(student , '/' , correct)
         if student != correct:
             first_score = first_score - 10
@@ -93,6 +132,18 @@ def print_fortype2():
     print(first_score)
     #100에서 10씩 뺴는방법
 
+for student in test:
+
+    for student in test:
+        math_score = print_fortype(student['math'], correct_answer['math'])
+        korean_score = print_fortype(student['korean'], correct_answer['korean'])
+        english_score = print_fortype(student['english'], correct_answer['english'])
+        science_score = print_fortype(student['science'], correct_answer['science'])
+
+        print("수학점수:", math_score)
+        print("국어점수:", korean_score)
+        print("영어점수:", english_score)
+        print("과학점수:", science_score)
 
 
 
