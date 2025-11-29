4a23969a9de04c051e2a3d3240c41edaad29910a
dohyunlee09 <dohyunlee0916@gmail.com>
2025-11-29 10:01:39 +0900

    업다운 만들기 커밋 1차

--- playground.py
+++ playground.py
@@ -1,31 +1,51 @@
 
 
-def print_times_table(number):
-    print(number, "*", 1, "=", number * 1)
+#def print_times_table(number):
+   # print(number, "*", 1, "=", number * 1)
 
-    print(number, "*", 2, "=", number * 2)
+    #print(number, "*", 2, "=", number * 2)
 
-    print(number, "*", 3, "=", number * 3)
+    #print(number, "*", 3, "=", number * 3)
 
-    print(number, "*", 4, "=", number * 4)
+    #print(number, "*", 4, "=", number * 4)
 
-    print(number, "*", 5, "=", number * 5)
+    #print(number, "*", 5, "=", number * 5)
 
-    print(number, "*", 6, "=", number * 6)
+    #print(number, "*", 6, "=", number * 6)
 
-    print(number, "*", 7, "=", number * 7)
+    #print(number, "*", 7, "=", number * 7)
 
-    print(number, "*", 8, "=", number * 8)
+    #print(number, "*", 8, "=", number * 8)
 
-    print(number, "*", 9, "=", number * 9)
+    #print(number, "*", 9, "=", number * 9)
 
+
+#while True:
+
+    #user_input = input("값을 입력하세요 : ")
+
+    #if user_input.lower() == "z":
+        #break
+
+    #print_times_table(int(user_input))
+#test
+import random
+
+def updown():
+    result:random.randrange(1 , 100
+
+                            )
 
 while True:
+    user_input:input("답을 입력하세요:")
+     int(user_input)
 
-    user_input = input("값을 입력하세요 : ")
+        if int(user_input) >random.randrange:
+        print("down")
 
-    if user_input.lower() == "z":
+        elif int(user_input) < random.randrange:
+        print("up")
+
+    if int(user_input) == random.randrange:
+        print("종료합니다.")
         break
-
-    print_times_table(int(user_input))
-#test
